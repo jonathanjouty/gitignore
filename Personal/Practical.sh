4da950586d7381cdfd79f0b6333175868e8a50b7
Jonathan Jouty <me@jonathanjouty.com>
2011-03-27 17:15:02 +0200

    Added more imports to custom script. See diff.

--- Personal/Practical.sh
+++ Personal/Practical.sh
@@ -1,21 +1,53 @@
 #!/bin/bash
 #
 
+function global_gen {
+echo "# --- Global ignores ---
+
+# Eclipse"
+cat ../Global/Eclipse.gitignore
+
+echo "
+# NetBeans"
+cat ../Global/NetBeans.gitignore
+
+echo "
+# OS X"
+cat ../Global/OSX.gitignore
+
+echo "
+# TextMate"
+cat ../Global/TextMate.gitignore
+
+}
+
 function gen {
-echo "# PDF Report
+echo "
+# --- Specific ignores ---
+
+# PDF Report
 report/report.pdf
 
 # ANSI C"
 cat ../ANSI-C.gitignore
+
 echo "
 
 # JAVA"
 cat ../Java.gitignore
+
 echo "
 
 # LaTeX"
 cat ../LaTeX.gitignore
+
+
+echo "
+
+# Coq"
+cat ../Coq.gitignore
 }
 
+global_gen
 gen
 
